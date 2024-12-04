fd50a2446969857b5fa456056baa489f9aa4680f
Tim Witkovsky <tim.witkov@gmail.com>
2024-12-03 21:31:43 -0500

    attmempting part 2 of day 2 again

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -1,13 +1,23 @@
-input_file = 'input'
-with open(input_file, 'r') as file:
+input_file = "input"
+with open(input_file, "r") as file:
     content = file.readlines()
 
 count = 0
+
+
 def find_safe(starter_line):
     line_array = list(map(int, starter_line.strip().split()))
-    if not (line_array == sorted(line_array)) and not line_array == sorted(line_array,reverse=True):
+    if not (line_array == sorted(line_array)) and not line_array == sorted(
+        line_array, reverse=True
+    ):
         return False
-    for i in range(0, len(line_array)- 1):
-        if 0 < abs(line_array[i] - line_array[i+1]) < 3:
-            return True
+    for i in range(0, len(line_array) - 1):
+        if not 1 <= abs(line_array[i] - line_array[i + 1]) <= 3:
+            return False
+    return True
 
+
+for line in content:
+    if find_safe(line):
+        count += 1
+print(count)
